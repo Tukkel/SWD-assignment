9e23e133fc8232e3d6b6e70b01e333f6a1f250ff
Jonas <jona813k@hotmail.com>
2024-04-14 16:04:40 +0200

    uml updated

--- uml.cs
+++ uml.cs
@@ -22,17 +22,17 @@ Animal-- > Visitor
 
 
 @startuml
-    note over Client: The instansiation of Caretaker, Turist and lion object
-Client -> Lion: accepts(visitor BigCaretaker)
+    note over Client: The instansiation of Caretaker, Turist and Lion object
+Client ->> Lion: accepts(visitor BigCaretaker)
 activate Client
 Lion -> BigCareTaker: visit(this { Lion object})
 activate BigCareTaker
-BigCareTaker -> Lion: Hunger is decreased
+BigCareTaker -->> Lion: Hunger is decreased
     deactivate BigCareTaker
-Client -> Lion: accepts(visitor Turist)
+Client ->> Lion: accepts(visitor Turist)
 Lion->Turist: visit(this { Lion object})
 activate Turist
-Turist -> Lion: Hunger is increased
+Turist -->> Lion: Hunger is increased
     deactivate Turist
     deactivate Client
     @enduml
